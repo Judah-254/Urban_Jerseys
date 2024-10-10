d3f96e8ef1dd978b2d3fa0a4a7eecfb07e22f14d
Judah wamae <Judahwamaen8@gmail.com>
2024-10-10 15:31:05 +0300

    database

--- php/db.php
+++ php/db.php
@@ -1,8 +1,18 @@
 <?php
-$servername = "localhost"; // XAMPP default server
-$username = "majhubcl_urbanjersey";        // Default MySQL username for XAMPP
-$password = "majhubcl_urbanjersey";            // Default password (leave empty)
-$dbname = "majhubcl_urbanjersey";  // Database name (create this in phpMyAdmin)
+
+if ($_SERVER['HTTP_HOST'] == 'localhost' || $_SERVER['HTTP_HOST'] == '127.0.0.1') {
+    // Local server (e.g., XAMPP)
+    $servername = "localhost"; // XAMPP default server
+    $username = "root";        // Default MySQL username for XAMPP
+    $password = "";            // Default password for XAMPP (usually empty)
+    $dbname = "urban_jerseys_db"; // Your local database name
+} else {
+    // Remote server
+    $servername = "localhost";    // Remote server's hostname (usually localhost too)
+    $username = "majhubcl_urbanjersey";        // Remote server MySQL username
+    $password = "majhubcl_urbanjersey";        // Remote server MySQL password
+    $dbname = "majhubcl_urbanjersey";          // Remote server database name
+}
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
